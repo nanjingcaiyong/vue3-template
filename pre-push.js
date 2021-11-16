287dc3d4f6d9506e94a5c026e913c146a9727795
yong.cai <yong.cai@kapeixi.com>
2021-11-16 13:57:16 +0800

    feat: chna

--- pre-push.js
+++ pre-push.js
@@ -1,6 +1,5 @@
 var gitBranchIs = require('git-branch-is');
 var shell = require('shelljs');
-
-gitBranchIs('test').then(() => {
-  shell.echo('npx standard-version');
+gitBranchIs('test').then((res) => {
+  res && shell.exec('npx standard-version');
 });
